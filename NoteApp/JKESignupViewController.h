da035c33fdb6f6ec30ebfef76d1fee707e14dd1b
krizskp <shalvindra@outlook.com>
2014-09-26 12:31:06 +1200

    Added Signup View Controller and signup logic

--- NoteApp/JKESignupViewController.h
+++ NoteApp/JKESignupViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface JKESignupViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+- (IBAction)signup:(id)sender;
 @end
--- NoteApp/JKESignupViewController.m
+++ NoteApp/JKESignupViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JKESignupViewController.h"
+#import <Parse/Parse.h>
 
 @interface JKESignupViewController ()
 
@@ -14,36 +15,40 @@
 
 @implementation JKESignupViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (void)viewDidLoad {
+	[super viewDidLoad];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (IBAction)signup:(id)sender {
+	NSString *username = [self.usernameTextField.text
+						  stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	NSString *email = [self.emailTextField.text
+						  stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	NSString *password = [self.passwordTextField.text
+						  stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	
+	if ([username length] == 0 || [password length] == 0 || [email length] == 0) {
+		UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+															message:@"You have to enter a username, password, and email"
+														   delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+		[alertView show];
+	} else {
+		PFUser *newUser = [PFUser user];
+		newUser.username = username;
+		newUser.password = password;
+		newUser.email = email;
+		
+		[newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+			if (error) {
+				UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+																	message:[error.userInfo objectForKey:@"error"]
+																   delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+				[alertView show];
+			} else {
+				[self.navigationController popToRootViewControllerAnimated:YES];
+			}
+		}];
+	}
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
